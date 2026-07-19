6d4e3a4be184ce5e6ed16250284c15254a0b29f8
Khalid-Dev <your-email@example.com>
2026-07-19 15:09:32 +0300

    update README and repository structure for v2 submission

--- AIN.Visitors.Mrz/obj/Debug/net10.0/AIN.Visitors.Mrz.AssemblyInfo.cs
+++ AIN.Visitors.Mrz/obj/Debug/net10.0/AIN.Visitors.Mrz.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AIN.Visitors.Mrz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf0b49726aea102ac5fdcb63e1c2f746786df2e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AIN.Visitors.Mrz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AIN.Visitors.Mrz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
